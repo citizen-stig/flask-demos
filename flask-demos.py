6a2046c87fbd58b4251253fb2e5a3f7d81ae2f88
Nikolay Golub <nikolay.v.golub@gmail.com>
2015-10-07 23:52:55 +0300

    Flask-admin experiments

--- flask-demos.py
+++ flask-demos.py
@@ -1,6 +1,11 @@
 from flask import Flask
-from models import db
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.model.form import InlineFormAdmin
+from models import db, User, UserNote
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'testing_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp.db'
 
 db.app = app
@@ -10,6 +15,25 @@ db.init_app(app)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+class ModelFormWithAuthorship(InlineFormAdmin):
+
+    def on_model_change(self, form, model):
+        user = User.query.first()
+        print('++++++')
+        print(user)
+        print(model)
+        print('++++++')
+        model.author = user
+
+
+class UserModelView(ModelView):
+    inline_models = (ModelFormWithAuthorship(UserNote),)
+
+
+admin = Admin(app, url='/admin', name='MyAdmin', template_mode='bootstrap3')
+admin.add_view(UserModelView(User, db.session))
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 # -*- encoding: utf-8 -*-
 from flask.ext.sqlalchemy import SQLAlchemy
-# from sqlalchemy import db.Column, db.Integer, db.String, DateTime, Boolean, Float
 
 db = SQLAlchemy()
 
@@ -8,4 +7,23 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
+    username = db.Column(db.String, unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class UserNote(db.Model):
+    __tablename__ = 'user_notes'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship(User,
+                           foreign_keys='UserNote.user_id',
+                           backref=db.backref('user_notes', order_by=id))
+    author = db.relationship(User,
+                             foreign_keys='UserNote.author_id',
+                             backref=db.backref('author_user_notes',
+                                                order_by=id))
