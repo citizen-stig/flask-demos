5dd22fbc0faefd2d4e502ff836c08887fe8823ed
Nikolay Golub <nikolay.v.golub@gmail.com>
2015-10-08 17:49:58 +0300

    Try to track model changes

--- flask-demos.py
+++ flask-demos.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.model.form import InlineFormAdmin
+from sqlalchemy import inspect
+from sqlalchemy.orm import class_mapper
 from models import db, User, UserNote
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -22,11 +24,15 @@ class ModelFormWithAuthorship(InlineFormAdmin):
     def on_model_change(self, form, model):
         user = User.query.first()
         print('++++++')
-        print(user)
-        print(model)
-        print('++++++')
-        model.author = user
-
+        print(model.body)
+        hista = inspect(model).attrs.body.history
+        print(hista.has_changes())
+        inspr = inspect(model)
+        attrs = class_mapper(model.__class__).column_attrs  # exclude relationships
+        for attr in attrs:
+            hist = getattr(inspr.attrs, attr.key).history
+            print(attr.key, hist.has_changes())
+        print('++++')
 
 class UserModelView(ModelView):
     inline_models = (ModelFormWithAuthorship(UserNote),)
